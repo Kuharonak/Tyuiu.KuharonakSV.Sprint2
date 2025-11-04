afd89866354cbf48b082b26d4e223758486618cc
СЕГА <skuhronak@gmail.com>
2025-11-04 20:36:42 +0300

    Task0

---
+++ Tyuiu.KuharonakSV.Sprint2.Task0.V16.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.KuharonakSV.Sprint2.Task0.V16.Lib;
+namespace Tyuiu.KuharonakSV.Sprint2.Task0.V16.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetCompareOperations()
+        {
+            DataService ds = new DataService();
+            int x = 1025;
+            int y = 275;
+            bool[] res = new bool[6];
+            res = ds.GetCompareOperations(x, y);
+            bool[] wait = new bool[] { true, false, true, false, true, false };
+
+            CollectionAssert.AreEqual(wait, res);
+        }
+    }
+}
