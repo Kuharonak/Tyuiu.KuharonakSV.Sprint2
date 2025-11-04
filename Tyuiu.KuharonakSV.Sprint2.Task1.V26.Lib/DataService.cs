4503f0b68f4ac5242651c37daccb17a0575e985a
СЕГА <skuhronak@gmail.com>
2025-11-04 21:18:51 +0300

    Task1

---
+++ Tyuiu.KuharonakSV.Sprint2.Task1.V26.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.KuharonakSV.Sprint2.Task1.V26.Lib
+{
+    public class DataService : ISprint2Task1V26
+    {
+        public bool[] GetLogicOperations(int a, int b, int c, int d)
+        {
+            bool[] res = new bool[6];
+            res[0] = (a < b) | (c == d);
+            res[1] = (a <= b) & (d < c);
+            res[2] = (c >= a) || (d > c);
+            res[3] = (d > a) && (c == b);
+            res[4] = !(!res[0]);
+            res[5] = (a + 23 > b) ^ (d + 20 > c);
+            return res;
+            
+
+        }
+    }
+}
---
+++ Tyuiu.KuharonakSV.Sprint2.Task1.V26.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.KuharonakSV.Sprint2.Task1.V26.Lib;
+namespace Tyuiu.KuharonakSV.Sprint2.Task1.V26.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetlogicOperations()
+        {
+            DataService ds = new DataService();
+            int a = 654;
+            int b = 671;
+            int c = 874;
+            int d = 137;
+            bool[] res = new bool[6];
+            res = ds.GetLogicOperations(a, b, c, d); 
+            bool[] wait = new bool[6] { true, true, true, false, true, false }; 
+            CollectionAssert.AreEqual(wait, res);
+        }
+    }
+}
