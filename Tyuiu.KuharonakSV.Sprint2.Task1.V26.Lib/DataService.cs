9bc15232e3d21a7321350f85944f9480e55787e3
СЕГА <skuhronak@gmail.com>
2025-11-04 21:19:41 +0300

    Task1

--- Tyuiu.KuharonakSV.Sprint2.Task1.V26.Lib/DataService.cs
+++ Tyuiu.KuharonakSV.Sprint2.Task1.V26.Lib/DataService.cs
@@ -11,7 +11,7 @@ namespace Tyuiu.KuharonakSV.Sprint2.Task1.V26.Lib
             res[2] = (c >= a) || (d > c);
             res[3] = (d > a) && (c == b);
             res[4] = !(!res[0]);
-            res[5] = (a + 23 > b) ^ (d + 20 > c);
+            res[5] = (a + 23 > b) ^ (d + 20 < c);
             return res;
             
 
