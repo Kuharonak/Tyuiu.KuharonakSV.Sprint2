58aba5d8b23acf9edf7cda6234003390b0317c75
СЕГА <skuhronak@gmail.com>
2025-11-07 01:13:01 +0300

    Task3

---
+++ Tyuiu.KuharonakSV.Sprint2.Task3.V25.Test/DataServiceTest.cs
@@ -0,0 +1,41 @@
+﻿using Tyuiu.KuharonakSV.Sprint2.Task3.V25.Lib;
+namespace Tyuiu.KuharonakSV.Sprint2.Task3.V25.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCondition1()
+        {
+            DataService ds = new DataService();
+            double x = 2;
+            double res = ds.Calculate(x);
+            double wait = 9.751;
+            Assert.AreEqual(wait, Math.Round(res, 3));
+        }
+        public void ValidCondition2()
+        {
+            DataService ds = new DataService();
+            double x = 0;
+            double res = ds.Calculate(x);
+            double wait = 0.333;
+            Assert.AreEqual(wait, Math.Round(res, 3));
+        }
+        public void ValidCondition3()
+        {
+            DataService ds = new DataService();
+            double x = 1;
+            double res = ds.Calculate(x);
+            double wait = 1;
+            Assert.AreEqual(wait, Math.Round(res, 3));
+        }
+        public void ValidCondition4()
+        {
+            DataService ds = new DataService();
+            double x = -30;
+            double res = ds.Calculate(x);
+            double wait = -3;
+            Assert.AreEqual(wait, Math.Round(res, 3));
+        }
+    }
+}
---
+++ Tyuiu.KuharonakSV.Sprint2.Task3.V25/Program.cs
@@ -0,0 +1,31 @@
+﻿using Tyuiu.KuharonakSV.Sprint2.Task3.V25.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #2 | Выполнил: Кухаронак С. В. | СМАРТБ-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #2                                                               *");
+        Console.WriteLine("* Тема: Вложенные операторы if - else                                     *");
+        Console.WriteLine("* Задание #3                                                              *");
+        Console.WriteLine("* Вариант #25                                                             *");
+        Console.WriteLine("* Выполнил: Кухаронак Сергей Витальевич | СМАРТБ-25-1                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая вычисляет требуемое значение функции Y с    *");
+        Console.WriteLine("* использованием вложенных операторов if - else, где пользователь вводит  *");
+        Console.WriteLine("* значение переменной X с клавиатуры.                                     *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("Введите значение переменной X: ");
+        double x = Convert.ToDouble(Console.ReadLine());
+        double res = ds.Calculate(x);
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("Значение функции = " + res);
+        Console.ReadKey();
+    }
+}
