36280e15b4d61b8eacbd7d5dcc8adfb5f66f06d3
СЕГА <skuhronak@gmail.com>
2025-11-10 00:35:29 +0300

    Task5

---
+++ Tyuiu.KuharonakSV.Sprint2.Task5.V7.Test/DataServiceTest.cs
@@ -0,0 +1,26 @@
+﻿using Tyuiu.KuharonakSV.Sprint2.Task5.V7.Lib;
+namespace Tyuiu.KuharonakSV.Sprint2.Task5.V7.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            Assert.AreEqual("Февраль", ds.FindMonthName(1990, 1));
+            Assert.AreEqual("Март", ds.FindMonthName(1990, 2));
+            Assert.AreEqual("Апрель", ds.FindMonthName(1990, 3));
+            Assert.AreEqual("Май", ds.FindMonthName(1990, 4));
+            Assert.AreEqual("Июнь", ds.FindMonthName(1990, 5));
+            Assert.AreEqual("Июль", ds.FindMonthName(1990, 6));
+            Assert.AreEqual("Август", ds.FindMonthName(1990, 7));
+            Assert.AreEqual("Сентябрь", ds.FindMonthName(1990, 8));
+            Assert.AreEqual("Октябрь", ds.FindMonthName(1990, 9));
+            Assert.AreEqual("Ноябрь", ds.FindMonthName(1990, 10));
+            Assert.AreEqual("Декабрь", ds.FindMonthName(1990, 11));
+            Assert.AreEqual("Январь", ds.FindMonthName(1990, 12));
+        }
+    }
+}
---
+++ Tyuiu.KuharonakSV.Sprint2.Task5.V7/Program.cs
@@ -0,0 +1,48 @@
+﻿using Tyuiu.KuharonakSV.Sprint2.Task5.V7.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #2 | Выполнил: Кухаронак С. В | СМАРТБ-25-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* спринт #2                                                               *");
+        Console.WriteLine("* Тема : Оператор Swith                                                   *");
+        Console.WriteLine("* Задание #5                                                              *");
+        Console.WriteLine("* Вариант #7                                                              *");
+        Console.WriteLine("* Выполнил: Кухаронак Сергей Витальевич | СМАРТБ-25-1                       *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу которая вычисляет месяц                              *");
+        Console.WriteLine("* Тернарного оператора                                                    *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("Введите номер месяца: ");
+        int numMouth = Convert.ToInt32(Console.ReadLine());
+
+        string res;
+
+        if ((numMouth < 0) || (numMouth > 12))
+        {
+            res = "Введение неверное значение!";
+        }
+        else
+        {
+            int startYear = Convert.ToInt32(Console.ReadLine());
+            res = "Это месяц: " + ds.FindMonthName(startYear, numMouth);
+
+
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Значение функции = " + res);
+
+            Console.ReadKey();
+        }
+}
