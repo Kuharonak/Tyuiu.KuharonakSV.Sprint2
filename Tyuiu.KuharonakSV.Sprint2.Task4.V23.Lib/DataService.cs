6ca329f9ccde2a88c50904715863783a4a0ea441
СЕГА <skuhronak@gmail.com>
2025-11-10 00:16:29 +0300

    Task4

---
+++ Tyuiu.KuharonakSV.Sprint2.Task4.V23.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.KuharonakSV.Sprint2.Task4.V23.Lib
+{
+    public class DataService : ISprint2Task4V23
+    {
+        public double Calculate(double x, double y)
+        {
+            double z = x / 2 < Math.Pow(y, 0.5) ? (Math.Pow(1 + (1 / Math.Pow(y, 2)), x)) : (y + 12 * x - (1 / x));
+            return Math.Round(z, 3);
+        }
+    }
+}
---
+++ Tyuiu.KuharonakSV.Sprint2.Task4.V23.Test/DataServiceTest.cs
@@ -0,0 +1,27 @@
+﻿using Tyuiu.KuharonakSV.Sprint2.Task4.V23.Lib;
+namespace Tyuiu.KuharonakSV.Sprint2.Task4.V23.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCondition1()
+        {
+            DataService ds  = new DataService();
+            double x = 2;
+            double y = 1;
+            double res = ds.Calculate(x, y);
+            double wait = 4;
+            Assert.AreEqual(wait, res);
+        }
+        public void ValidCondition2()
+        {
+            DataService ds = new DataService();
+            double x = 1;
+            double y = 2;
+            double res = ds.Calculate(x, y);
+            double wait = 13;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
