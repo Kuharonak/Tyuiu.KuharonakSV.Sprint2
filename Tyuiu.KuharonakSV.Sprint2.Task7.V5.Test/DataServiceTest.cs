afdbf08c9ef1811e5ce29d4d445d34d48ffc94fe
СЕГА <skuhronak@gmail.com>
2025-11-13 13:01:27 +0300

    Task7

---
+++ Tyuiu.KuharonakSV.Sprint2.Task7.V5.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Tyuiu.KuharonakSV.Sprint2.Task7.V5.Lib;
+namespace Tyuiu.KuharonakSV.Sprint2.Task7.V5.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCheckDotInShadedArea()
+        {
+            DataService ds = new DataService();
+
+            double x = 0;
+            double y = 0.5;
+
+            bool res = ds.CheckDotInShadedArea(x, y);
+
+            bool wait = true;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.KuharonakSV.Sprint2.Task7.V5/Program.cs
@@ -0,0 +1,47 @@
+﻿using Tyuiu.KuharonakSV.Sprint2.Task7.V5.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #1 | Выполнил: Кухаронак Сергей Витальевич | СМАРТБ-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #2                                                               *");
+        Console.WriteLine("* Тема: Базовые навыки работы в С#                                        *");
+        Console.WriteLine("* Задание #7                                                              *");
+        Console.WriteLine("* Вариант #5                                                              *");
+        Console.WriteLine("* Выполнил: Кухаронак Сергей Витальевич | СМАРТБ25-1                      *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+        Console.WriteLine("* вычисляет, и печатает результат на экране                               *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("Введите значение переменной Х: ");
+        double x = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введите значение переменной Y: ");
+        double y = Convert.ToDouble(Console.ReadLine());
+
+        bool res = ds.CheckDotInShadedArea(x, y);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        if (res)
+        {
+            Console.WriteLine("Точка находится в заштрихованной области");
+        }
+        else
+        {
+            Console.WriteLine("Точка не находится в закрашенной области");
+        }
+
+
+        Console.ReadKey();
+    }
+}
